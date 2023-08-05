c979208e8dd2b5a783a8b1a68f70c57c32645259
Spencer <eklund.spencer@outlook.com>
2023-08-05 12:02:48 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, testLetter) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(testLetter.toLowerCase())
+  );
+}
+
+function matchName(drivers, info) {
+  return drivers.filter((driver) => {
+    const nameMatches = driver.name.toLowerCase().includes(info.toLowerCase());
+    const hometownMatches = driver.hometown
+      .toLowerCase()
+      .includes(info.toLowerCase());
+    return nameMatches || hometownMatches;
+  });
+}
